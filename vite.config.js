4e8a9dfb59a925b547dac62477272c73d133b971
Alexis Vignot <alexisvignot@hotmail.fr>
2024-12-08 12:34:37 +0100

    fix: vercel

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,9 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
-  build: {
-    rollupOptions: {
-      external: ['vuex'],  // Ajoutez ici les dépendances externes nécessaires
-    },
-  },
   resolve: {
     alias: {
-      '@': '/src',  // Assurez-vous que l'alias est correct pour votre structure de projet
+      '@': '/src', // Assurez-vous que l'alias est correct
     },
   },
 });
