720695db25c7b83af3f0cbe1014b05bc2873d46b
Alexis Vignot <alexisvignot@hotmail.fr>
2024-12-08 11:58:55 +0100

    fix: vercel

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: 'Spiecraft/',
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+  base: '/',
+  plugins: [react()],
+});
